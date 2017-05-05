d0df06d6f1bed245ad88b91a644b83871afcceff
Henry <henry.lin@tallan.com>
2017-05-05 17:40:04 -0400

    added constant strings to resources and scaffolded the notif service3

--- app/src/main/java/rillin/notificaway2/NotificawayService.java
+++ app/src/main/java/rillin/notificaway2/NotificawayService.java
@@ -1,9 +1,50 @@
 package rillin.notificaway2;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.support.v4.app.NotificationManagerCompat;
 
 /** rihllin 5/2017. */
 
 public class NotificawayService extends NotificationListenerService {
 
+    private NotificawayServiceReceiver receiver;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (!hasNotificationAccess()) {
+            // todo prompt user to give notification access to this service
+        }
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+    }
+
+    private boolean hasNotificationAccess() {
+        return NotificationManagerCompat.getEnabledListenerPackages(this).contains(this.getClass().getPackage().getName());
+    }
+
+    class NotificawayServiceReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    }
 }
