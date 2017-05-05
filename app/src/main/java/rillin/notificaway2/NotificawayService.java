2092b32a77d4f709d85c3c066d332ceff5779a8a
Henry <henry.lin@tallan.com>
2017-05-05 17:24:25 -0400

    added notiflistener service

---
+++ app/src/main/java/rillin/notificaway2/NotificawayService.java
@@ -0,0 +1,9 @@
+package rillin.notificaway2;
+
+import android.service.notification.NotificationListenerService;
+
+/** rihllin 5/2017. */
+
+public class NotificawayService extends NotificationListenerService {
+
+}
